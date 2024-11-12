c57ebefcba4a56cbfc3d15740ea37fe7c978c00f
emiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-11-12 20:49:37 -0300

    config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,84 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        light: '#F0ECFA',
+        medium: '#D3D9E1',
+        dark: '#3A2E55',
+        card: '#F7F9FC',
+        panel: '#E5EAF2',
+        accent: '#C1DFF2'
+      },
+      textColor: {
+        primary: '#1C1B1F',
+        secondary: '#4A4A57',
+        'secondary-light': '#D0D0D5',
+        body: '#33333D',
+        muted: '#707070'
+      }
+    },
+    colors: {
+      primary: {
+        100: '#AC92E0',
+        200: '#9E80DB',
+        300: '#906ED6',
+        400: '#825CD1',
+        500: '#744ACC',
+        600: '#6843B8',
+        700: '#5D3BA3',
+        800: '#51348F',
+        900: '#462C7A'
+      },
+      secondary: {
+        100: '#7E888F',
+        200: '#69747D',
+        300: '#53606A',
+        400: '#3E4C58',
+        500: '#283845',
+        600: '#24323E',
+        700: '#202D37',
+        800: '#1C2730',
+        900: '#182229'
+      },
+      tertiary: {
+        100: '#66CAED',
+        200: '#4DC1EA',
+        300: '#33B9E7',
+        400: '#1AB0E4',
+        500: '#00A7E1',
+        600: '#0096CB',
+        700: '#0086B4',
+        800: '#00759E',
+        900: '#006487'
+      },
+      black: '#000000',
+      white: '#FFFFFF',
+      gray: {
+        200: '#CDCDCD',
+        300: '#AEAEAE',
+        400: '#A4A4A4',
+        500: '#9A9A9A',
+        600: '#8B8B8B',
+        700: '#7B7B7B',
+        800: '#4D4D4D'
+      },
+      success: {
+        100: '#C4DE93',
+        500: '#9DC84B',
+        900: '#5E782D'
+      },
+      warning: {
+        100: '#FFD394',
+        500: '#FFB54D',
+        900: '#996D2E'
+      },
+      error: {
+        100: '#DF8888',
+        500: '#CA3838',
+        900: '#792222'
+      }
+    },
     screens: {
       mobile: '390px',
       tablet: '768px',
